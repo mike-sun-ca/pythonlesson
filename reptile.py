23f05d846f239151814fc82ccf2455cc29cb2ab9
Mike.sun <72673692+mike-sun-ca@users.noreply.github.com>
2023-06-21 04:33:06 -0700

    got temp

--- reptile.py
+++ reptile.py
@@ -1,5 +1,18 @@
 import requests
+import json
 
-
-response= requests.get("https://www.theweathernetwork.com/ca/weather/british-columbia/langley")
-print(response)
+headers={
+    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36"
+}
+urlAddress = "http://api.weatherstack.com/current" \
+"?access_key=e456ff10006f8eeec0f7d944692696b0" \
+"&query=Langley" \
+"&units=m" \
+"& language = en" \
+"& callback = MY_CALLBACK"
+response= requests.get(urlAddress,headers)
+print (urlAddress)
+print(response.text)
+print("*******************")
+jsonStr = json.loads(response.text)
+print("Current Temperature: " , jsonStr['current']["temperature"] , "Celsius degree")
